355f331a78f9b9741be07f34943d7ea8ecd288ab
Muniyappasamy M <muni.mostwanted@gmail.com>
2023-05-12 04:54:54 -0700

    feat: Added swagger documentation for get all books rest api

--- src/main/java/com/bnpp/fortis/developmentbooks/controller/DevelopmentBooksController.java
+++ src/main/java/com/bnpp/fortis/developmentbooks/controller/DevelopmentBooksController.java
@@ -2,8 +2,10 @@ package com.bnpp.fortis.developmentbooks.controller;
 
 import com.bnpp.fortis.developmentbooks.model.Book;
 import com.bnpp.fortis.developmentbooks.service.DevelopmentBooksService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +20,12 @@ public class DevelopmentBooksController {
 
     private final DevelopmentBooksService developmentBooksService;
 
+
+    @ApiOperation(value = "Get list of Books in the Store ", response = Iterable.class, tags = "getAllBooks")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success|OK"),
+            @ApiResponse(code = 403, message = "forbidden!!!"),
+            @ApiResponse(code = 404, message = "not found!!!")})
     @GetMapping("${developmentbooks.endpoints.getallbooks}")
     public List<Book> getallbooks() {
         return developmentBooksService.getAllBooks();
