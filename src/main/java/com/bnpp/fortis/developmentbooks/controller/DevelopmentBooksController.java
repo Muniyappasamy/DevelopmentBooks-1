5f0e23313e61cfd9b434dc16b6b4d2c8088bcc0a
Muniyappasamy M <muni.mostwanted@gmail.com>
2023-05-12 04:39:21 -0700

    refactor: moved api strings to properties and added requiredArg constructor for better readability

--- src/main/java/com/bnpp/fortis/developmentbooks/controller/DevelopmentBooksController.java
+++ src/main/java/com/bnpp/fortis/developmentbooks/controller/DevelopmentBooksController.java
@@ -2,6 +2,7 @@ package com.bnpp.fortis.developmentbooks.controller;
 
 import com.bnpp.fortis.developmentbooks.model.Book;
 import com.bnpp.fortis.developmentbooks.service.DevelopmentBooksService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/developmentbooks")
+@RequestMapping("${developmentbooks.controller.path}")
+@RequiredArgsConstructor
 public class DevelopmentBooksController {
 
-    @Autowired
-    private DevelopmentBooksService developmentBooksService;
 
-    @GetMapping("/getallbooks")
+    private final DevelopmentBooksService developmentBooksService;
+
+    @GetMapping("${developmentbooks.endpoints.getallbooks}")
     public List<Book> getallbooks() {
         return developmentBooksService.getAllBooks();
     }
