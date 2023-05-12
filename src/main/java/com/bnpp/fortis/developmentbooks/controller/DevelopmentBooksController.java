3b209e183b12f6e9bc30d774256b63f9947a157a
Muniyappasamy M <muni.mostwanted@gmail.com>
2023-05-12 04:25:19 -0700

    refactor: get all rest api will return list of all books

--- src/main/java/com/bnpp/fortis/developmentbooks/controller/DevelopmentBooksController.java
+++ src/main/java/com/bnpp/fortis/developmentbooks/controller/DevelopmentBooksController.java
@@ -1,15 +1,23 @@
 package com.bnpp.fortis.developmentbooks.controller;
 
+import com.bnpp.fortis.developmentbooks.model.Book;
+import com.bnpp.fortis.developmentbooks.service.DevelopmentBooksService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/developmentbooks")
 public class DevelopmentBooksController {
 
+    @Autowired
+    private DevelopmentBooksService developmentBooksService;
+
     @GetMapping("/getallbooks")
-    public String getallbooks() {
-        return "All Books";
+    public List<Book> getallbooks() {
+        return developmentBooksService.getAllBooks();
     }
 }
--- src/test/java/com/bnpp/fortis/developmentbooks/controller/DevelopmentBooksControllerTest.java
+++ src/test/java/com/bnpp/fortis/developmentbooks/controller/DevelopmentBooksControllerTest.java
@@ -1,10 +1,12 @@
 package com.bnpp.fortis.developmentbooks.controller;
 
+import com.bnpp.fortis.developmentbooks.service.DevelopmentBooksService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -20,6 +22,9 @@ class DevelopmentBooksControllerTest {
     private DevelopmentBooksController developmentBooksController;
 
 
+    @MockBean
+    private DevelopmentBooksService developmentBooksService;
+    
     @Test
     @DisplayName("development book controller instance should not be null")
     void developmentControllershouldNotBeNull() {
